9698f25850965c38dd61ba793a7b031858379c3c
Davidr1002 <david.rau@kellogg.northwestern.edu>
2022-01-21 19:32:45 +0000

    progress 1

--- kmdb.sql
+++ kmdb.sql
@@ -67,14 +67,99 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS top_cast;
 
 -- Create new tables, according to your domain model
--- TODO!
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year_released INTEGER,
+    mpaa_rating TEXT,
+    director TEXT
+);
+CREATE TABLE top_cast (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    name TEXT,
+    role TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
--- Use hard-coded foreign key IDs when necessary
--- TODO!
+-- Use hard-coded foreign key IDs when necessary => TO CHECK
+INSERT INTO movies(
+    title,
+    year_released,
+    mpaa_rating,
+    director
+)
+VALUES 
+  ("Batman Begins",
+  "2005",
+  "PG-13",
+  "Christopher Nolan"),
+   ("The Dark Knight",
+  "2008",
+  "PG-13",
+  "Christopher Nolan"),
+  ("The Dark Knight Rises",
+  "2012",
+  "PG-13",
+  "Christopher Nolan")
+;
+INSERT INTO top_cast(
+    title,
+    name,
+    role
+)
+VALUES 
+  ("Batman Begins",
+  "Christian Bale",
+  "Bruce Wayne"),
+    ("Batman Begins",
+  "Michael Caine",
+  "Alfred"),  
+  ("Batman Begins",
+  "Liam Neeson",
+  "Ra's Al Ghul"),
+  ("Batman Begins",
+  "Katie Holmes",
+  "Rachel Dawes"),  
+  ("Batman Begins",
+  "Gary Oldman",
+  "Commissioner Gordon"),
+    ("The Dark Knight",
+  "Christian Bale",
+  "Bruce Wayne"),
+   ("The Dark Knight",
+  "Heath Ledger",
+  "Joker"),
+   ("The Dark Knight",
+  "Aaron Eckhart",
+  "Harvey Dent"),
+   ("The Dark Knight",
+  "Michael Caine",
+  "Alfred"),
+   ("The Dark Knight",
+  "Maggie Gyllenhaal",
+  "Rachel Dawes"),
+   ("The Dark Knight Rises",
+  "Christian Bale",
+  "Bruce Wayne"),
+    ("The Dark Knight Rises",
+  "Gary Oldman",
+  "Commissioner Gordon"),
+     ("The Dark Knight Rises",
+  "Tom Hardy",
+  "Bane"),
+     ("The Dark Knight Rises",
+  "Joseph Gordon-Levitt",
+  "John Blake"),
+     ("The Dark Knight Rises",
+  "Anne Hathaway",
+  "Selina Kyle")
+;
+
 
 -- Prints a header for the movies output
 .print "Movies"
@@ -82,7 +167,10 @@
 .print ""
 
 -- The SQL statement for the movies output
--- TODO!
+SELECT title, year_released, mpaa_rating, director
+FROM movies
+;
+
 
 -- Prints a header for the cast output
 .print ""
@@ -92,4 +180,6 @@
 
 
 -- The SQL statement for the cast output
--- TODO!
+SELECT title, name, role
+FROM top_cast
+;
